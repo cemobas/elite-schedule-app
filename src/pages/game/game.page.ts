fd6f4c45e715a271371baa136d40db9e993ea057
CEM ONUR BAS <cemonurbas@CEM-MacBook-Pro.local>
2017-01-01 23:12:09 +0100

    Game page has been updated in a nice format (section in training: Putting It All Together).

--- src/pages/game/game.page.ts
+++ src/pages/game/game.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { TeamHomePage } from '../pages';
+
+import { MapPage, TeamHomePage } from '../pages';
 import { EliteApi } from '../../shared/shared';
+declare var window: any;
 
 @Component({
-  templateUrl: 'game.page.html'
+  templateUrl: 'game.page.html',
 })
 export class GamePage {
-  game: any;
+  game: any = {};
 
   constructor(
     public nav: NavController,
@@ -16,6 +18,7 @@ export class GamePage {
 
   ionViewDidLoad(){
     this.game = this.navParams.data;
+    this.game.gameTime = Date.parse(this.game.time);
   }
 
   teamTapped(teamId){
@@ -24,4 +27,18 @@ export class GamePage {
     this.nav.push(TeamHomePage, team); 
   }
 
+  goToDirections(){
+    let tourneyData = this.eliteApi.getCurrentTourney();
+    let location = tourneyData.locations[this.game.locationId];
+    window.location = `geo:${location.latitude},${location.longitude};u=35;`;
+  }
+
+  goToMap(){
+    this.nav.push(MapPage, this.game);
+  }
+
+  isWinner(score1, score2){
+    //return Number(score1) > Number(score2);
+    return Number(score1) > Number(score2) ? 'secondary' : '';
+  }
 }
